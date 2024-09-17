d62fcf87c0ccee9c3a903b34dc098f20bacbe13b
Dobras Timothy <GumekTV@gmail.com>
2024-09-17 18:23:48 +0300

    cron job ?

--- app/api/trigger-spin/route.ts
+++ app/api/trigger-spin/route.ts
@@ -4,17 +4,10 @@ import { pusher } from "@/lib/pusher/pusher";
 import { processBetsAfterSpin } from "@/lib/wheel/game-state";
 import { getNumberColorName } from "@/utils/roulette/roulette-functions";
 
-const SECRET_KEY = process.env.SPIN_TRIGGER_SECRET_KEY || "";
-
-// Function to generate a random spin result using the secret key
+// Function to generate a random spin result
 function generateRandomSpin() {
-  const hash = crypto
-    .createHmac("sha256", SECRET_KEY)
-    .update(crypto.randomBytes(16))
-    .digest("hex");
-
-  const randomValue = parseInt(hash, 16);
-  const spinResult = randomValue % 52; // Get a number between 0 and 52
+  // Generate a truly random integer between 0 and 51
+  const spinResult = crypto.randomInt(0, 52);
   return spinResult;
 }
 
@@ -43,6 +36,7 @@ export async function GET() {
 
     return NextResponse.json({ message: "Spin triggered", result: spinResult });
   } catch (error) {
+    console.error("Error during spin:", error);
     return NextResponse.json(
       { message: "Failed to trigger spin", error: error },
       { status: 500 },
